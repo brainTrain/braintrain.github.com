a4bf0de062b6b35eccc87cbfd2db20338403f994
Brian Schroeder <abductee@gmail.com>
2011-09-06 21:42:19 -0700

    some stuff

--- static/js/lomaprieta.js
+++ static/js/lomaprieta.js
@@ -4,5 +4,11 @@ $(document).ready( function() {
         $('.window').attr('src', 'http://' + $(this).attr('url'));
     });
     
+    $('.nav').hover( function() {
+            $(this).css('background-color', '#FFCC99'); 
 
+      }, function() {
+
+            $(this).css('background-color', '#CC99CC'); 
+    });
 });
