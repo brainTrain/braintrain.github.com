ecc3d8d3f0dfa2aea2655731f283cf52eb43745f
Brian Schroeder <abductee@gmail.com>
2011-11-14 23:38:36 -0800

    diagnostic

--- static/js/lomaprieta.js
+++ static/js/lomaprieta.js
@@ -3,21 +3,17 @@ $(document).ready( function() {
     $('.nav').click(function() {
         $('#content').load($(this).attr('url'));
         if($(this).attr('id') == 'log') {
-            console.log('thisbelog');
             $('#content').css('overflow-y', 'hidden');
             $('#content').css('overflow-x', 'hidden');
 
         } else {
-            console.log('thisaint');
             $('#content').css('overflow-y', 'auto');
             $('#content').css('overflow-x', 'hidden');
         
         }
     });
     $('#diagnostics').click( function() {
-        trace('loading diagnostics');
         $('#content').load('load/diagnostics.html');
-
     });
     
     $('.nav').hover( function() {
