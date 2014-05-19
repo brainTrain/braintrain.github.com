7d2e16dd166a11d12e82ee364a212adf16deadf6
braintrain <abductee@gmail.com>
2014-05-19 09:08:04 -0700

    flesh out descriptions a bit
    * display description and linx on detail page

--- media/js/services.js
+++ media/js/services.js
@@ -21,7 +21,12 @@ angular.module('brainTrainProjects.services', [])
                     }
                 ],
                 "summary": "Make the internet drunk! (chrome plugin)",
-                "description": ["chrome plugin", "css animations (not super performant)"]
+                "description": {
+                    "words": [
+                        "chrome plugin to make teh webz drunk", 
+                        "boozy webz accomplished by a combination of css animations and jQuery animations"
+                    ]
+                }
             },    
             {
                 "name":"fuckingrainbows",
@@ -39,7 +44,21 @@ angular.module('brainTrainProjects.services', [])
                     }
                 ],
                 "summary": "Invade the web with fabulous CSS3 rainbows! (bookmarklet)",
-                "description": ["websockets consumer (read: frontend only... er kinda)", "jQuery/CSS selector approximation of the js this object", "websockets + this-ish selector = remote clicking! (Don't worry, most people don't get what that means either)"]
+                "description": {
+                    "words": [
+                        "clicking on html objects toggles a class which adds animated scrolling CSS3 rainbows",
+                        "websockets consumer using pusher.com", 
+                        "jQuery/CSS selector approximation of the js this object", 
+                        "created a function called ajaxThis() which takes the js this object and maps out a flat approximation of a this selector (read more in my loggly blogpost link)",
+                        "websockets + ajaxThis selector = remote clicking! (meaning two or more people on the same URL will see the rainbows others clicked into creation)"
+                    ],
+                    "links": [
+                        {
+                            "title": "My Loggly blogpost about my ajaxThis function",
+                            "url": "https://www.loggly.com/blog/ajax-this/"
+                        }
+                    ]
+                }
             },
             {
                 "name":"usslomaprieta",
@@ -57,7 +76,9 @@ angular.module('brainTrainProjects.services', [])
                     }
                 ],
                 "summary": "Almost LCARS UI for an SF Star Trek club!",
-                "description": ["super static site using CSS for LCARS flavorz"]
+                "description": {
+                    "words": ["super static site using CSS for LCARS flavorz"]
+                }
             },
             {
                 "name":"brainnewstuff",
@@ -75,7 +96,15 @@ angular.module('brainTrainProjects.services', [])
                     }
                 ],
                 "summary": "My digital code/config/command line notepad.",
-                "description": ["django site for keeping track of technical notes", "sendmail install/config", "SSL cert"]
+                "description": {
+                    "words": [
+                        "user based django site for keeping track of technical notes", 
+                        "note options are command line, config, code or description",
+                        "user content routed via username in the subdomain",
+                        "currently hosted on my linode box with a few other projects",
+                        "has wildcard SSL cert and sendmail routes"
+                    ]
+                }
             },
             {
                 "name":"iamed",
@@ -93,7 +122,22 @@ angular.module('brainTrainProjects.services', [])
                     }
                 ],
                 "summary": "Twilio audio blog for a friend with a non-smart cell phone.",
-                "description": ["hacker news comment about I am Ed <a href=\"https://news.ycombinator.com/item?id=7655742\">https://news.ycombinator.com/item?id=7655742</a>", "django site for Ed's audio thoughts and images", "sendmail with email endpoints for a non-smartphone control (i.e. whitelisting email addresses, texting images to web server)", "PIL(Python Image Library) for image saving and resizing", "twilio so Ed can just call a number to submit an audio blog type post"]
+                "description": {
+                    "words": [
+                        "django site for Ed's audio thoughts and images", 
+                        "sendmail with email endpoints for a non-smartphone control (i.e. whitelisting email addresses, texting images to web server)", 
+                        "PIL(Python Image Library) for image saving and resizing", 
+                        "twilio so Ed can just call a number to submit an audio blog type post",
+                        "integrated sendmail, PIL and django for posts that originate from text messages",
+                        "has SSL cert"
+                    ],
+                    "links": [
+                        {
+                            "title": "hacker news comment about I am Ed",
+                            "url": "https://news.ycombinator.com/item?id=7655742"
+                        }
+                    ]
+                }
             },
             {
                 "name":"atoddswithclarity",
@@ -111,7 +155,12 @@ angular.module('brainTrainProjects.services', [])
                     }
                 ],
                 "summary": "Personal indieweb integrated blog!",
-                "description": ["django site for blogging/posting to facebook/twitter using indieweb practices"]
+                "description": {
+                    "words": [
+                        "django site for blogging/posting to facebook/twitter using indieweb practices",
+                        "has SSL cert and sendmail routes"
+                    ]
+                }
             },
             {
                 "name":"fuckingsf",
@@ -129,7 +178,9 @@ angular.module('brainTrainProjects.services', [])
                     }
                 ],
                 "summary": "Floating splash site!",
-                "description": ["playing around with splash pages/CSS keyframing (it floats!)"]
+                "description": {
+                    "words": ["playing around with splash pages/CSS keyframing (it floats!)"]
+                },
             },
             {
                 "name":"nerdpaper",
@@ -142,13 +193,27 @@ angular.module('brainTrainProjects.services', [])
                     }
                 ],
                 "summary": "Highcharts series controls!",
-                "description": ["playing around with highcharts, controls allow users to compare multiple bar graphs by sliding one over the other."]
+                "description": { 
+                    "words": [
+                        "playing around with highcharts, controls allow users to compare multiple bar graphs by sliding one over the other."
+                    ]
+                }
             },
             {
                 "name":"ecosystem",
                 "title":"Ecosystem Shit",
                 "summary": "All projects here are either hosted on github or a box I built on linode or digital ocean.",
-                "description": ["So far I've proven to myself that I can do anything I want with one box. My next ops-type goal is to learn how to configure varnish to cache all of the requests from all of the projects I'm currently hosting."]
+                "description": {
+                    "words": [
+                        "So far I've proven to myself that I can do anything I want with one box. My next ops-type goal is to learn how to configure varnish to cache all of the requests from all of the projects I'm currently hosting."
+                    ],
+                    "links": [
+                        {
+                            "title": "most of my opsy/sysadmin knowledge is recorded on my brainnewstuff account",
+                            "url": "#/brainnewstuff"
+                        }
+                    ]
+                }
             }
         ]
         if(projectName) {
